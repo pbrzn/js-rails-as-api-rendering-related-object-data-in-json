30fdbee0d28e740c9504c036fe04038f0f89a1bf
Patrick Brennan <pbrennanmusic@gmail.com>
2021-10-09 13:47:06 -0400

    commit tree 5cdb64997da832e568b29691275882012756a89e

---
+++ app/controllers/sightings_controller.rb
@@ -0,0 +1,12 @@
+class SightingsController < ApplicationController
+  def show
+    sighting = Sighting.find_by(id: params[:id])
+    if sighting
+    render json: sighting.to_json(:include => {
+        :bird => {:only => [:name, :species]},
+        :location => {:only => [:latitude, :longitude]}
+      }, :except => [:created_at, :updated_at])
+    else
+      render json: {message: "Sighting not found"}
+  end
+end
